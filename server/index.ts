c199a6a6197bc57ef592110533842356df68db73
Chrillaz <totalchriz@gmail.com>
2022-02-13 01:36:49 +0100

    express entrypoint

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,28 @@
-const path = require('path');
-const express = require('express');
-require('dotenv').config();
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import * as bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
+import { staticRouter, entityRouter } from './routes';
+
+dotenv.config();
 
 const app = express()
 
 const port = process.env.PORT
 
-app.use(express.static(path.join(__dirname, '../public')))
+staticRouter(path.join(__dirname, '../public'), app);
 
-app.get('/', (req, res) => {
-  res.sendFile('/index.html');
-})
+const apiVersion = process.env.API_VERSION;
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors())
+
+app.use(`/api/${apiVersion}/entities`, entityRouter(app));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Playground listening on port ${port}`)
 })
