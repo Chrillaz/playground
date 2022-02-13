630dabb0a7afcd5ed1c1fba1b52f95c15fc17a24
Chrillaz <totalchriz@gmail.com>
2022-02-14 00:12:03 +0100

    message reducer

---
+++ client/reducers/messageReducer.ts
@@ -0,0 +1,49 @@
+import { IMessage } from '@/services/messages';
+type Actions = 'MESSAGES_INIT' | 'MESSAGE_ADD' | 'MESSAGE_UPDATE' | 'MESSAGE_REMOVE';
+
+export type TMessages = Record<string, IMessage>;
+
+export interface IAction {
+    type: Actions;
+    payload?: Record<string, any>;
+    init?: IMessage[];
+}
+
+export const messageReducer = (state: TMessages, action: IAction) => {
+
+    const { type, payload, init } = action;
+
+    switch(type) {
+        case 'MESSAGES_INIT': {
+
+            return {
+                ...init.reduce((map, message) => ({...map, [message.id]: message}), {})
+            }
+        }
+        case 'MESSAGE_ADD': {
+
+            return {
+                ...state,
+                [payload.id]: payload
+            }
+        }
+        case 'MESSAGE_UPDATE': {
+            
+            return {
+                ...state,
+                [payload.id]: payload
+            }
+        }
+        case 'MESSAGE_REMOVE': {
+
+            let {[payload.id]: omit, ...rest} = state;
+
+            return {
+                ...rest
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
