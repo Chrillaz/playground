3e2202bf5ae91d7d0681fcbff53050f967f84bc7
Chrillaz <totalchriz@gmail.com>
2022-02-15 09:18:41 +0100

    styles

--- client/components/message-appender/message-appender.tsx
+++ client/components/message-appender/message-appender.tsx
@@ -1,41 +1,63 @@
-import React, { FC } from 'react';
-import { IMessage } from '@/services/messages';
-import { useForm, FormProvider } from 'react-hook-form';
-import { InputControl } from '../input-control/input-control';
-import { Button } from '@mui/material';
+import { IMessage } from "@/services/messages";
+import { Button, Container } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import React, { FC } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { InputControl } from "../input-control/input-control";
 
 interface IProps {
-    onSubmit: (message: IMessage) => void;
+	onSubmit: (message: IMessage) => void;
 }
 
-export const MessageAppender: FC<IProps> = ({onSubmit}) => {
-
-    const formMethods = useForm();
-
-    const handleSubmit = () => {
-
-        if ( typeof onSubmit === 'function' ) {
-
-            onSubmit({...formMethods.getValues()} as IMessage);
-        }
+const useStyles = makeStyles({
+	multilineColor: {
+		"& .MuiInputBase-input": {
+			color: "white",
+		},
+	},
+    actionContainer: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        marginTop: 8
     }
+});
 
-    return (
-        <FormProvider {...formMethods}>
-            <InputControl 
-                name="message"
-                variant="outlined"
-                size="small"
-                multiline
-            />
-            <Button
-                size="small"
-                variant="contained"
-                color="primary"
-                onClick={handleSubmit}
-            >
-                Submit
-            </Button>
-        </FormProvider>
-    )
-}
+export const MessageAppender: FC<IProps> = ({ onSubmit }) => {
+	const classes = useStyles();
+
+	const formMethods = useForm();
+
+	const handleSubmit = () => {
+		if (typeof onSubmit === "function") {
+			onSubmit({ ...formMethods.getValues() } as IMessage);
+            formMethods.reset({ message: '' });
+		}
+	};
+
+	return (
+		<Container maxWidth="md">
+			<FormProvider {...formMethods}>
+				<InputControl
+					focused
+					multiline
+                    fullWidth
+					name="message"
+					variant="outlined"
+					size="small"
+					InputProps={{
+						className: classes.multilineColor,
+					}}
+				/>
+				<div className={classes.actionContainer}>
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={handleSubmit}
+					>
+						Submit
+					</Button>
+				</div>
+			</FormProvider>
+		</Container>
+	);
+};
