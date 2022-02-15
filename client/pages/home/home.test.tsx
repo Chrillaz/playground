9d6a3ab8a8327a46aa451bd5bc94f99165f8a9ea
Chrillaz <totalchriz@gmail.com>
2022-02-15 13:51:11 +0100

    naive integration test...

--- client/pages/home/home.test.tsx
+++ client/pages/home/home.test.tsx
@@ -1,13 +1,74 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import Home from '.'
+import { ThemeProvider } from '@mui/styles';
+import { createTheme, Theme } from '@mui/material';
+import { act } from 'react-dom/test-utils';
 
 describe('<Home />', () => {
 
-    it('Renders with hellow world', () => {
+    const theme = {
+        palette: {
+            mode: 'dark'
+        }
+    } as Theme;
 
-        render(<Home />);
+    const mockMessage = { id: 'some-id', message: 'Hello World'};
 
-        expect(screen.getByText('Hello World')).toBeInTheDocument
+    jest.mock('@/services/messages', () => ({
+        getAll: () => jest.fn()
+            .mockResolvedValueOnce([])
+            .mockResolvedValueOnce([mockMessage]),
+        getById: () => jest.fn().mockResolvedValueOnce(mockMessage),
+        create: () => jest.fn().mockResolvedValueOnce(mockMessage),
+    }))
+
+    jest.mock('@/services/api', () => ({
+        get: jest.fn(),
+        post: jest.fn()
+    }))
+
+    beforeEach(() => jest.resetAllMocks);
+
+    it('Renders with Submit a message.', () => {
+
+        render(
+            <ThemeProvider theme={createTheme(theme)}>
+                <Home />
+            </ThemeProvider>
+        );
+
+        expect(screen.getByText('Submit a message.')).toBeInTheDocument
+    })
+
+    it('Can post a message', () => {
+
+        render(
+            <ThemeProvider theme={createTheme(theme)}>
+                <Home />
+            </ThemeProvider>
+        );
+
+        const message = 'Hello World';
+
+        const textArea = screen.getByPlaceholderText('What is on your mind?');
+
+        const submit = screen.getByRole('button');
+
+        expect(textArea).toBeInTheDocument;
+
+        expect(submit).toBeInTheDocument;
+        
+        textArea.setAttribute('value', message);
+
+        act(() => {
+
+            fireEvent.click(submit);
+        })
+
+        waitFor(() => {
+
+            expect(screen.getByText(message)).toBeInTheDocument;
+        })
     })
 })
