089036b39df85394d90d970d8b78021782a0e9ec
Chrillaz <totalchriz@gmail.com>
2022-04-03 12:43:23 +0200

    doc types

--- client/utilities/local-storage/local-storage.ts
+++ client/utilities/local-storage/local-storage.ts
@@ -1,14 +1,24 @@
+/**
+ * 
+ * @param key - string
+ * @param value any
+ */
 export const storageWrite = <T = string>(key: string, value: T): void => {
 
-    if ('localStorage' in window) {
+    if ('Storage' in window) {
 
         localStorage.setItem(key, JSON.stringify(value));
     }
 }
 
+/**
+ * 
+ * @param key - string
+ * @returns - any
+ */
 export const storageRead = <T = any>(key: string): T => {
 
-    if ('localStorage' in window) {
+    if ('Storage' in window) {
 
         return JSON.parse(localStorage.getItem(key));
     }
