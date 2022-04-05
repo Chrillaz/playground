98c04e351dc4435e65fa22c5ee41f63227e829e2
Chrillaz <totalchriz@gmail.com>
2022-04-05 22:46:46 +0200

    added spec

--- test/jest.config.js
+++ test/jest.config.js
@@ -8,11 +8,13 @@ module.exports = {
     moduleNameMapper: {
         "@/(.*)$": "<rootDir>/client/$1",
         "@components": "<rootDir>/client/components/index.ts",
-        '\\.(css)$': '<rootDir>/test/__mocks__/style-mock.ts',
+        '\\.(css)$': '<rootDir>/test/__mocks__/mui-styles.ts',
     },
     setupFilesAfterEnv: ["<rootDir>/test/setup.js"],
     testEnvironment: "jsdom",
-    testMatch: ["<rootDir>/client/**/*.test.+(ts|tsx)"],
+    testMatch: [
+        "<rootDir>/client/**/*.(spec|test).+(ts|tsx)"
+    ],
     testURL: 'http://localhost:3000',
     transform: {
         ".*\\.(ts|tsx)$": "ts-jest",
