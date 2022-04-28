741b9e0f39b9e5daa3e508ad93ca67ae9457db4f
Chrillaz <totalchriz@gmail.com>
2022-04-28 09:52:00 +0200

    changed db connection and removed som redundant compose keys

--- .entrypoint/app_entrypoint.sh
+++ .entrypoint/app_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/wait-for-it $MYSQL_DATABASE:$MYSQL_PORT -- \
+/usr/bin/wait-for-it $MYSQL_HOST:$MYSQL_PORT -- \
     npx prisma generate --schema=./services/prisma/schema.prisma \
     && npx prisma migrate dev --schema=./services/prisma/schema.prisma \
     && npm run api:dev 
