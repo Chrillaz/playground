32ef383d94b7a39528d48a3fdef21fda63ced571
Chrillaz <totalchriz@gmail.com>
2022-05-01 09:38:18 +0200

    exception test

--- services/client/src/utilities/http-handle/http-handle.test.ts
+++ services/client/src/utilities/http-handle/http-handle.test.ts
@@ -1,15 +1,27 @@
 import { httpHandle } from './http-handle';
 import { AxiosResponse } from 'axios';
 
+
 const mockRequest = (): Promise<AxiosResponse> => new Promise((resolve) => {
 
     setTimeout(() => resolve({ data: true } as AxiosResponse), 1000);
 })
 
-test('returns data', async () => {
+test('it returns data', async () => {
 
     const [error, data] = await httpHandle(mockRequest());
 
     expect(error).toBe(undefined);
     expect(data).toBe(true);
+})
+
+test('it throws axios Cancel', async () => {
+
+    const [error, data] = await httpHandle(
+        mockRequest(),
+        { alive: false }
+    )
+
+    expect(data).toBe(undefined);
+    expect(error.message).toMatch('Canceled on unmounted component.')
 })
